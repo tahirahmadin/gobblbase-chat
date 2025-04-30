3ce80bd11c547ae8f3c6870a2e9e27579d0209c4
Tahir Ahmad <tahirahmad.in@gmail.com>
2025-04-30 13:02:18 +0530

    changew

--- src/components/chatbotComponents/ChatSection.tsx
+++ src/components/chatbotComponents/ChatSection.tsx
@@ -29,8 +29,7 @@ export default function ChatSection({
       className="flex-1 overflow-y-auto p-2"
       style={{
         backgroundColor: theme.isDark ? "#1c1c1c" : "#e9e9e9",
-        paddingBottom: "100px",
-        height: "calc(100vh - 200px)",
+        paddingBottom: "80px",
       }}
     >
       {activeScreen === "chat" && (
